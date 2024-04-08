951a62702198f91bd7e77a63e71eba07c819ec9d
= <=>
2024-04-08 17:02:35 -0500

    Episode 13 - Eager Loading and the N+1 Problem

--- thirty-days/resources/views/jobs.blade.php
+++ thirty-days/resources/views/jobs.blade.php
@@ -1,17 +1,18 @@
 <x-layout>
-<x-slot:heading>Job Listings</x-slot:heading>
+    <x-slot:heading>Job Listings</x-slot:heading>
 
-<ul>
+    <div class="space-y-4">
 
-@foreach ($jobs as $job)
+        @foreach ($jobs as $job)
 
-    <li>
-        <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div class="font-bold text-blue-500 text-xs uppercase">{{ $job->employer->name }}</div>
+            <div>   
             <strong>{{$job['title']}}:</strong> Pays {{ $job['salary'] }} per year
-        </a>    
-    </li>
-    
-    @endforeach
-</ul>
+            </div>
+            </a>
+
+        @endforeach
+    </div>
 
 </x-layout>
