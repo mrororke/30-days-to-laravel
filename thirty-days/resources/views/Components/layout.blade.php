daf79a91f9b7586002c64539c0bdbcc6e46adcbe
= <=>
2024-04-08 17:05:53 -0500

    Corrected issue with Nav

--- thirty-days/resources/views/Components/layout.blade.php
+++ thirty-days/resources/views/Components/layout.blade.php
@@ -19,8 +19,9 @@
             <div class="ml-10 flex items-baseline space-x-4">
              
               <x-nav-link href="/"  :active="request()->is('/')">Home</x-nav-link>
-              <x-nav-link href="jobs"  :active="request()->is('/jobs')">Jobs</x-nav-link>
-              <x-nav-link href="contact" :active="request()->is('/contact')">Contact</x-nav-link>
+              <x-nav-link href="/jobs"  :active="request()->is('jobs')">Jobs</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+
             </div>
           </div>
         </div>
