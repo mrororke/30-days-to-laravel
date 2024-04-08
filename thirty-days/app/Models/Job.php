8dced5fb79d38bd4af42c7ff2cc855ab436be2d9
= <=>
2024-04-08 16:21:28 -0500

    Episode 11 - Two Key Eloquent Relationships

--- thirty-days/app/Models/Job.php
+++ thirty-days/app/Models/Job.php
@@ -12,5 +12,10 @@ class Job extends Model{
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
    
 }
